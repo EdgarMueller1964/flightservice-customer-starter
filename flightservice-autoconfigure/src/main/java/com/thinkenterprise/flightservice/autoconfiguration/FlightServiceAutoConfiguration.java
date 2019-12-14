9c627522659160fc342790674b57ff2a68c76f60
edgar müller <edgar.mueller@msg.group>
2019-12-14 17:33:43 +0100

    flight service starter demo .
    -- not using parent customer starter
    -- library, autoconfigure and starter separate projects
    -- FlightService Configuration Properties (taxRate)

--- flightservice-autoconfigure/src/main/java/com/thinkenterprise/flightservice/autoconfiguration/FlightServiceAutoConfiguration.java
+++ flightservice-autoconfigure/src/main/java/com/thinkenterprise/flightservice/autoconfiguration/FlightServiceAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.thinkenterprise.flightservice.configuration.FlightServiceConfigurationProperties;
 import com.thinkenterprise.flightservice.service.FlightService;
 import com.thinkenterprise.flightservice.service.FlightServiceImpl;
 
@@ -12,6 +13,13 @@ import com.thinkenterprise.flightservice.service.FlightServiceImpl;
 @ConditionalOnClass(FlightService.class)
 public class FlightServiceAutoConfiguration {
 
+
+	@Bean
+	public FlightServiceConfigurationProperties fsProperties() {
+		return new FlightServiceConfigurationProperties();
+	}
+	
+	
     //conditional bean creation
     @Bean
     @ConditionalOnMissingBean
--- flightservice-library/src/main/java/com/thinkenterprise/flightservice/service/FlightServiceImpl.java
+++ flightservice-library/src/main/java/com/thinkenterprise/flightservice/service/FlightServiceImpl.java
@@ -2,10 +2,16 @@ package com.thinkenterprise.flightservice.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.thinkenterprise.flightservice.configuration.FlightServiceConfigurationProperties;
 import com.thinkenterprise.flightservice.model.FlightPrice;
 
 public class FlightServiceImpl implements FlightService {
 
+	@Autowired
+	FlightServiceConfigurationProperties fsProperties;
+	
 	@Override
 	public double totalPrice(List<FlightPrice> flightPrices) {
 		double totalPrice = 0.0;
@@ -13,7 +19,11 @@ public class FlightServiceImpl implements FlightService {
 			totalPrice = totalPrice + flightPrice.getPrice();
 		}
 		
-		return totalPrice;	
+		double taxRate = 1.0;
+		if (fsProperties.getTaxRate() != 0.0f)
+			taxRate = fsProperties.getTaxRate();
+		
+		return totalPrice * taxRate;	
 	}
 
 }
